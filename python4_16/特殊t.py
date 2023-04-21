fe40fc33374ba24b9d955a48a733744d566455ab
1659850144@qq.com <1659850144@qq.com>
2023-04-21 23:00:19 +0800

    一分为八

---
+++ python4_16/特殊t.py
@@ -0,0 +1,11 @@
+import os
+# 创建文件夹
+# if not os.path.exists('D:\\mn\\mnmx\\时髦气质美女周迅优雅成熟小气写真图片/pqlj4h01arf.jpg" style="font-size: 12px;'):
+#     os.makedirs('D:\\mn\\mnmx\\时髦气质美女周迅优雅成熟小气写真图片/pqlj4h01arf.jpg" style="font-size: 12px;')
+
+class test:
+    def test1(self,num1):
+        if num1==2:
+            return print('该数为2')
+        print("依然只想")
+test().test1(2)
